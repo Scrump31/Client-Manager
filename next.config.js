ac65ae742fcda726344a4c3e5dc6f0da6c6f9413
Scottie Crump <sncrump29@gmail.com>
2021-11-08 09:17:47 -0500

    feat: :sparkles: add QA environment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
 
-module.exports = {
+const nodeEnv = process.env.NODE_ENV || 'development'
+const config = {
   env: {
-    username: process.env.USERNAME,
-    mongodb: process.env.MONGODB_URI
+    username: process.env.USERNAME
   }
 }
+
+if (nodeEnv === 'qa') {
+  config.env['mongodb'] = process.env.MONGODB_URI_QA
+} else {
+  config.env['mongodb'] = process.env.MONGODB_URI_DEV
+}
+
+module.exports = config
